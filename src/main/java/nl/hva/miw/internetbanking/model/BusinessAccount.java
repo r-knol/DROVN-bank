6a08dbc52427c2f03960a5db6c9c1c0c1ba84bed
Richard Knol <richard.knol@live.nl>
2020-12-08 17:13:02 +0100

    merge conflict opgelost na branche inloggen mergen in master3.0

--- src/main/java/nl/hva/miw/internetbanking/model/BusinessAccount.java
+++ src/main/java/nl/hva/miw/internetbanking/model/BusinessAccount.java
@@ -2,9 +2,7 @@ package nl.hva.miw.internetbanking.model;
 
 import java.util.List;
 
-public class BusinessAccount extends Account {
+//public class BusinessAccount extends Account {
 
-    public BusinessAccount(long rekeningID, double saldo, String IBAN, List<Transaction> transactionHistorie) {
-        super(rekeningID, saldo, IBAN, transactionHistorie);
-    }
-}
+
+//}
