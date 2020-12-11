2ea146a3673730c31ccd8105a23a06af78ec31ca
Richard Knol <richard.knol@live.nl>
2020-12-11 15:33:06 +0100

    merge conflicten proberen op te lossen, werkt nog niet helemaal

--- src/main/java/nl/hva/miw/internetbanking/controller/LoginController.java
+++ src/main/java/nl/hva/miw/internetbanking/controller/LoginController.java
@@ -2,6 +2,7 @@ package nl.hva.miw.internetbanking.controller;
 
 import nl.hva.miw.internetbanking.data.dto.CustomerAccountDTO;
 import nl.hva.miw.internetbanking.model.Account;
+import nl.hva.miw.internetbanking.model.Customer;
 import nl.hva.miw.internetbanking.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,44 +28,34 @@ public class LoginController {
     }
 
     @PostMapping("/login") //@Richard Knol
-    public String handleLogin(@RequestParam String userName, String password, Model model) { // Gaat iets doen met ingevoerde G en W
-
+    public String handleLogin(@RequestParam String userName, String password, Model model) {
 
         CustomerAccountDTO c = customerService.getCustomerByUsernameAndPassword(userName, password);
         if (c != null) {
             List<Account> accountList = c.getAccountList();
+
             for (Account a : accountList) {
                 CustomerAccountDTO b = customerService.getCustomersByAccount(a.getAccountID());
                 model.addAttribute("accountWithCustomerList", b);
+
                 System.out.println(b.getCustomerList());
+
+                // lijstje klaarzetten voor namen van customers:
+                List<String> customerNameList = new ArrayList<>();
+
+                // voor alle klanten uit het lijstje:
+                for (Customer klant : b.getCustomerList()) {
+                    // maak een String van de customerName met printNameCustomer methode uit CustomerService:
+                    String customerName = customerService.printNameCustomer(klant.getId());
+                    // voeg deze toe aan het lijstje met customer-namen:
+                    customerNameList.add(customerName);
+                }
+                // lijstje met namen toevoegen aan Model:
+                model.addAttribute("customerNameList", customerNameList);
             }
-        CustomerAccountDTO c = customerService.getCustomerAccountOverview(userName, password);
-        List<Account> accountList = c.getAccountList();
-
-        for (Account a : accountList) {
-            CustomerAccountDTO b = customerService.getCustomersByAccount(a.getAccountID());
-            model.addAttribute("accountWithCustomerList", b);
-
-            System.out.println(b.getCustomerList());
-
-            // lijstje klaarzetten voor namen van customers:
-            List<String> customerNameList = new ArrayList<>();
-
-            // voor alle klanten uit het lijstje:
-            for (Customer klant : b.getCustomerList()) {
-                // maak een String van de customerName met printNameCustomer methode uit CustomerService:
-                String customerName = customerService.printNameCustomer(klant.getId());
-                // voeg deze toe aan het lijstje met customer-namen:
-                customerNameList.add(customerName);
-            }
-            // lijstje met namen toevoegen aan Model:
-            model.addAttribute("customerNameList", customerNameList);
-        }
-
             model.addAttribute("customerWithAccountOverview", c);
             return "pages/rekeningoverzicht";
         }
         return "pages/foutpagina";
-
     }
 }
