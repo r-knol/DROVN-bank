65235f0e2a7e20f6f870ea9fe390fa9543bdc42e
dleertouwer <daniel.leertouwer@hva.nl>
2020-12-11 02:53:27 +0100

    Ik zei toch dat ik pas ging slapen als het werkte...

---
+++ src/main/java/nl/hva/miw/internetbanking/data/dto/RegistrationDto.java
@@ -0,0 +1,39 @@
+package nl.hva.miw.internetbanking.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import nl.hva.miw.internetbanking.model.LegalPerson;
+import nl.hva.miw.internetbanking.model.NaturalPerson;
+
+@Getter
+@Setter
+public class RegistrationDto {
+
+    private static final String EMPTY_STRING = "";
+
+    private LegalPerson personLegal;
+    private NaturalPerson personPrivate;
+    private String username;
+    private String password;
+    private String passwordConfirmation;
+
+    public RegistrationDto() {
+        this.personLegal = new LegalPerson();
+        this.personPrivate = new NaturalPerson();
+        this.username = EMPTY_STRING;
+        this.password = EMPTY_STRING;
+        this.passwordConfirmation = EMPTY_STRING;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistrationDto{" +
+                "personLegal=" + personLegal +
+                ", personPrivate=" + personPrivate +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", passwordConfirmation='" + passwordConfirmation + '\'' +
+                '}';
+    }
+
+}
