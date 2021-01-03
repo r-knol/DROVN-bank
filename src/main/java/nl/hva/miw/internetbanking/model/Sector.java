4a94d990f115f3f3d0ea2103000f707e148a9521
dleertouwer <daniel.leertouwer@hva.nl>
2021-01-03 21:45:19 +0100

    Added sector

--- src/main/java/nl/hva/miw/internetbanking/model/Sector.java
+++ src/main/java/nl/hva/miw/internetbanking/model/Sector.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Sector {
 
+    NOT_SPECIFIED("Niet gespecificeerd"),
     RETAIL("Detailhandel, groothandel en autohandel"),
     HOSPITALITY("Horeca"),
     EDUCATION("Onderwijs"),
