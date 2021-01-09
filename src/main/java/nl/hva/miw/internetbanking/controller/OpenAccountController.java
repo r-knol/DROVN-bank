63b3fbfaef33a9d17c97b7c956a5d15bf28931db
VeroniekG <75933085+VeroniekG@users.noreply.github.com>
2021-01-09 13:59:11 +0100

    Toevoegen methode voor AJAX

--- src/main/java/nl/hva/miw/internetbanking/controller/OpenAccountController.java
+++ src/main/java/nl/hva/miw/internetbanking/controller/OpenAccountController.java
@@ -9,19 +9,31 @@ import nl.hva.miw.internetbanking.service.CustomerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 @Controller
+@SessionAttributes("customer")
 public class OpenAccountController {
 
     private AccountService accountService;
     private CustomerService customerService;
     private Logger logger = LoggerFactory.getLogger(AccountOverviewController.class);
+
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     public OpenAccountController(AccountService accountService, CustomerService customerService) {
@@ -30,21 +42,40 @@ public class OpenAccountController {
         logger.warn("New OpenAccountController.");
     }
 
-   @GetMapping("/openaccount/{id}")
-    public ModelAndView showNewAccount(@PathVariable(value = "id") long id, ModelAndView model){
-        Optional<Customer> customer = customerService.getCustomerById(id);
+    @GetMapping("/openaccount")
+    public String showNewAccount(@ModelAttribute("customer") Customer customer, Model model) {
         Account newAccount = new Account();
-        model.addObject("account", newAccount);
-        model.addObject("customer", customer.get());
-        model.setViewName("pages/open-account");
-       logger.info(" " + model);
-       return model;
+        model.addAttribute("account", newAccount);
+        model.addAttribute("customer", customer);
+        model.addAttribute("nameCurrentCus", customerService.printNameCustomer(customer.getCustomerID()));
+        logger.info(" getMapping openaccount" + model);
+        return "pages/open-account";
     }
 
     @PostMapping("/saveaccount")
-    public ModelAndView saveAccount (@ModelAttribute ("account") Account account, @ModelAttribute ("customer") Customer customer){
+    public String saveAccount(@ModelAttribute("account") Account account, @ModelAttribute("customer") Customer customer) {
         logger.info(" Dit is huidige: " + account + customer);
         accountService.saveNewAccount(account, customer);
-        return new ModelAndView("pages/account-overview");
+        return "pages/account-overview";
+    }
+
+    private Account getAccount(@RequestParam String iban) {
+        Account account;
+        try {
+            account = jdbcTemplate.queryForObject("SELECT iban FROM account iban =?", new ibanMapper(), iban);
+        } catch (EmptyResultDataAccessException emptyRS) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Iban niet gevonden", emptyRS);
+        }
+        return account;
+    }
+}
+
+
+class ibanMapper implements RowMapper<Account> {
+
+    @Override
+    public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Account(rs.getString("iban"));
     }
 }
