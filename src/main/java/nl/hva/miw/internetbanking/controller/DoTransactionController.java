689810710ed4736b852a92b371e553a48e58bbad
Nina van Loo <nina.van.loo@hva.nl>
2021-01-10 15:41:29 +0100

    1 import was niet gedaan, nu wel

--- src/main/java/nl/hva/miw/internetbanking/controller/DoTransactionController.java
+++ src/main/java/nl/hva/miw/internetbanking/controller/DoTransactionController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
