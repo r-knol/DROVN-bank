23867c233dee35021ae9ab5d3238d24c2a783a5d
VeroniekG <75933085+VeroniekG@users.noreply.github.com>
2021-01-14 12:43:55 +0100

    InsertScript klantenbestand

---
+++ Documentatie/SQL/InsertScript_klantenbestand/4_employee.sql
@@ -0,0 +1,10 @@
+INSERT INTO `employee` (`employeeID`,`userName`,`password`,`firstName`,`surName`,`role`) VALUES (1,'Lu2','0gG#Wm=bh','Ed','Hopper','ACCOUNTMANAGER');
+INSERT INTO `employee` (`employeeID`,`userName`,`password`,`firstName`,`surName`,`role`) VALUES (2,'Patricia037','ea#KU%sds','Brad','Brown','ACCOUNTMANAGER');
+INSERT INTO `employee` (`employeeID`,`userName`,`password`,`firstName`,`surName`,`role`) VALUES (3,'Andrew','mc#TeOGK6','Guo','Jiminez','ACCOUNTMANAGER');
+INSERT INTO `employee` (`employeeID`,`userName`,`password`,`firstName`,`surName`,`role`) VALUES (4,'Rob','''yFLrncC3','Christine','Blount','ACCOUNTMANAGER');
+INSERT INTO `employee` (`employeeID`,`userName`,`password`,`firstName`,`surName`,`role`) VALUES (5,'Luis360','%D$xY''nBf','Juri','Meterson','ACCOUNTMANAGER');
+INSERT INTO `employee` (`employeeID`,`userName`,`password`,`firstName`,`surName`,`role`) VALUES (6,'Mads3','zP35uJ17.','Hens','Dittrich','HEAD_PRIVATE');
+INSERT INTO `employee` (`employeeID`,`userName`,`password`,`firstName`,`surName`,`role`) VALUES (7,'Cathy','34d:3&"A7','John','Chwatal','HEAD_LEGAL');
+
+
+
