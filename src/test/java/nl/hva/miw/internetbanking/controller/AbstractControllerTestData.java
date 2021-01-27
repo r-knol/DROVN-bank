cfbb1c7558e5571dd3872aecf61c05fb5405c57f
dleertouwer <daniel.leertouwer@hva.nl>
2021-01-27 12:45:32 +0100

    CustomerControllerTest

--- src/test/java/nl/hva/miw/internetbanking/controller/AbstractControllerTestData.java
+++ src/test/java/nl/hva/miw/internetbanking/controller/AbstractControllerTestData.java
@@ -4,56 +4,48 @@ import nl.hva.miw.internetbanking.model.CustomerType;
 import nl.hva.miw.internetbanking.model.LegalPerson;
 import nl.hva.miw.internetbanking.model.NaturalPerson;
 import nl.hva.miw.internetbanking.model.Sector;
-import org.springframework.test.context.ActiveProfiles;
 
-import java.util.ArrayList;
-
-@ActiveProfiles("test-nodb")
-public abstract class AbstractControllerTest {
+public abstract class AbstractControllerTestData {
     
-    public static final String USERNAME = "ppost";
-    public static final long CUSTOMER_ID = 999L;
+    protected static final String USERNAME = "ppost";
+    protected static final long CUSTOMER_ID = 999L;
     private static final String PASSWORD = "Aa!123";
     private static final String INITIALS = "P.";
     private static final String FIRSTNAME = "Pieter";
     private static final String PREPOSITION = "";
     private static final String SURNAME = "Post";
     private static final String DATE_OF_BIRTH = "1981-09-16";
-    private static final String SOCIAL_SECURITY = "123456789";
+    protected static final String SOCIAL_SECURITY_NUMBER = "123456789";
     private static final String EMAIL = "pieter@post.nl";
     private static final String PHONE = "06-12345678";
     private static final String POSTAL_CODE = "1234 AB";
     private static final String HOME_NUMBER = "1A-F";
     private static final String STREET = "Briefstraat";
     private static final String RESIDENCE = "Poststad";
-    private static final CustomerType BUSINESS_CUSTOMER_TYPE = CustomerType.LEGAL;
     private static final String COMPANY = "Post Unlimited";
-    private static final long KVK_NUMBER = 12345678L;
-    private static final Sector SECTOR = Sector.TRANSPORT;
+    protected static final long KVK_NUMBER = 12345678L;
     private static final String VAT_NUMBER = "NL12345678B01";
     
     protected final NaturalPerson privateCustomer;
     protected final LegalPerson businessCustomer;
     
-    protected AbstractControllerTest() {
+    protected AbstractControllerTestData() {
         this.privateCustomer = setUpPrivateCustomer();
         this.businessCustomer = setUpBusinessCustomer();
     }
-    
     
     private NaturalPerson setUpPrivateCustomer() {
         return NaturalPerson.builder()
                 .customerID(CUSTOMER_ID)
                 .userName(USERNAME)
                 .password(PASSWORD)
-                .accounts(new ArrayList<>())
                 .customerType(CustomerType.NATURAL)
                 .initials(INITIALS)
                 .firstName(FIRSTNAME)
                 .preposition(PREPOSITION)
                 .surName(SURNAME)
                 .dateOfBirth(DATE_OF_BIRTH)
-                .socialSecurityNumber(SOCIAL_SECURITY)
+                .socialSecurityNumber(SOCIAL_SECURITY_NUMBER)
                 .email(EMAIL)
                 .phone(PHONE)
                 .postalCode(POSTAL_CODE)
@@ -68,6 +60,7 @@ public abstract class AbstractControllerTest {
                 .customerID(CUSTOMER_ID)
                 .userName(USERNAME)
                 .password(PASSWORD)
+                .customerType(CustomerType.LEGAL)
                 .companyName(COMPANY)
                 .kvkNumber(KVK_NUMBER)
                 .sector(Sector.TRANSPORT)
